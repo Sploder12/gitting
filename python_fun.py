e4c478935720f29a3079354eb5bed559b1ebe93f
Devin <devrod514@gmail.com>
2019-04-20 13:39:01 -0400

    this is a message by the way.

--- python_fun.py
+++ python_fun.py
@@ -1,18 +1,13 @@
-print("hello")
-print("Anotha one")
-print("Hello Trevor!")
-print("I broke it!")
-
-x = 20
-if(x == 20):
-	print("X was 20!")
-	x = 4000
-	print("X is now 4000!")
-if(x == 0):
-	print("9+10=21!")
+def evenodd(number):
+	if number%2==0:
+		print("Probably even.")
+	else:
+		print("Maybe Odd.")
 
 def wordcount(sentence):
   word = sentence.split()
   print(len(word))
 
 wordcount("this is a sentence")
+
+evenodd(5)
